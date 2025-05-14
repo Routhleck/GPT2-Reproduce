79167acc1e6feb67ab1e5ea7ecfe6744a0683881
routhleck <sichaohe@stu.pku.edu.cn>
2025-05-14 09:09:10 +0800

    [2401111538] Implement weight sharing between transformer and language model head in GPT-2

--- train_gpt2.py
+++ train_gpt2.py
@@ -91,6 +91,9 @@ class GPT(nn.Module):
 
         self.lm_head = nn.Linear(config.n_embd, config.vocab_size, bias=False)
 
+        # weight sharing scheme
+        self.transformer.wte.weight = self.lm_head.weight
+
     def forward(self, idx, targets=None):
         device = idx.device
         b, t = idx.size()
